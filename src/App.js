2dc8d7c2ca2455fea4dcf1205facebd1b206d092
Dom Taylor <dominick.taylor@gmail.com>
2020-12-07 19:17:47 +0000

    mailchimp integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LottieComponent from './components/lottie.comp';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 import './App.css';
 
@@ -7,10 +8,12 @@ function App() {
 	const url1 = 'https://assets6.lottiefiles.com/packages/lf20_8uHQ7s.json';
 	const width = 400;
 	const height = 400;
-
+    const URL = process.env.REACT_APP_MAILCHIMP_URL
 	return (
 		<main>
 			<h1>Website Coming Soon</h1>
+			<h2>Stay Informed</h2>
+			<MailchimpSubscribe url={URL} />
 			<LottieComponent url={url1} height={height} width={width} name={''}/>
 			<br />
 		</main>
